6ff932f2b7657c7fbccc9b1959a9118d7980512d
YuCai18 <1512875381@qq.com>
2025-04-10 23:25:46 +0800

    把消息体封装成一个数据结构，争取一次发送，一次接收

--- EasyClient/EasyTcpClient1.cpp
+++ EasyClient/EasyTcpClient1.cpp
@@ -6,7 +6,9 @@
 enum CMD
 {
 	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
 	CMD_LOGOUT,
+	CMD_LOGOUT_RESULT,
 	CMD_ERROR
 };
 //消息头
@@ -16,25 +18,45 @@ struct DataHeader
 	short cmd;
 };
 //DataPackage
-struct Login
+struct Login :public DataHeader
 {
+	Login() {
+		dataLength = sizeof(Login);
+		cmd = CMD_LOGIN;
+	}
 	char userName[32];
 	char PassWord[32];
 };
 
-struct LoginResult
+struct LoginResult :public DataHeader
 {
+	LoginResult()
+	{
+		dataLength = sizeof(LoginResult);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
 	int result;
-
 };
 
-struct Logout
+struct Logout : public DataHeader
 {
+	Logout()
+	{
+		dataLength = sizeof(Logout);
+		cmd = CMD_LOGOUT;
+	}
 	char userName[32];
 };
 
-struct LogoutResult
+struct LogoutResult : public DataHeader
 {
+	LogoutResult()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
 	int result;
 
 };
@@ -75,28 +97,23 @@ int main() {
 			break;
 		}
 		else if(0 == strcmp(cmdBuf, "login")){
-			Login login = {"CaiYu","123"};
-			DataHeader dh = { sizeof(Login),CMD_LOGIN};
 			//5.向服务器发送请求命令
-			send(_sock, (const char*)&dh,sizeof(dh),0);
+			Login login;
+			strcpy(login.userName,"CaiYu");
+			strcpy(login.PassWord, "123");
 			send(_sock, (const char*)&login, sizeof(login), 0);
 			//接收服务器返回的消息
-			DataHeader retHeader = {};
 			LoginResult loginRet = {};
-			recv(_sock,(char*)&retHeader,sizeof(retHeader),0);
 			recv(_sock, (char*)&loginRet, sizeof(loginRet), 0);
 			printf("LoginResult:%d \n",loginRet.result);
 		}
 		else if (0 == strcmp(cmdBuf, "logout")) {
-			Logout logout = {};
-			DataHeader dh = {sizeof(logout),CMD_LOGOUT};
 			//向服务器发送请求命令
-			send(_sock, (const char*)&dh, sizeof(dh), 0);
+			Logout logout;
+			strcpy(logout.userName,"CaiYu");
 			send(_sock, (const char*)&logout, sizeof(logout), 0);
 			//接收服务器返回的消息
-			DataHeader retHeader = {};
-			LogoutResult logoutRet = {};
-			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			LogoutResult logoutRet;
 			recv(_sock, (char*)&logoutRet, sizeof(logoutRet), 0);
 			printf("LogoutResult :%d \n", logoutRet.result);
 		}
